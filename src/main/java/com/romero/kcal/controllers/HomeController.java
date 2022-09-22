8aaf7684bdea38549a102e76a630476004fe2eba
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-09-22 00:28:17 -0500

    api key in application.properties

--- src/main/java/com/romero/kcal/controllers/HomeController.java
+++ src/main/java/com/romero/kcal/controllers/HomeController.java
@@ -1,12 +1,20 @@
 package com.romero.kcal.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Value("${KEY}")
+    public String KEY;
+    @Value("${HOST}")
+    public String HOST;
     @GetMapping("/")
-    public String homepage(){
+    public String homepage(Model model){
+        model.addAttribute("KEY",KEY);
+        model.addAttribute("HOST", HOST);
         return "index";
     }
 }
