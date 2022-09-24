ab5288869beee58e0e1cd0c6afc277acc132addb
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-09-23 19:30:32 -0500

    fix

--- src/main/java/com/romero/kcal/controllers/CalculatorController.java
+++ src/main/java/com/romero/kcal/controllers/CalculatorController.java
@@ -15,29 +15,20 @@ public class CalculatorController {
     public String MACRO_HOST;
 
     @GetMapping("/calculator")
-    public String macrCounter(Model model){
-
-        model.addAttribute("KEY",KEY);
-        model.addAttribute("MACRO_HOST", MACRO_HOST);
-
-        return "calculator";
+    public String macrCounter(Model model) {
+        return "/calculator/calculator";
     }
 
     @PostMapping("/calculator")
-    public String macroCounter(Model model, @RequestParam(name = "activity-lv") String activity, @RequestParam(name = "goal") String goal, @RequestParam(name = "age") String age, @RequestParam(name = "gender") String gender, @RequestParam(name = "height") String height, @RequestParam(name = "weight") String weight){
-       model.addAttribute("age",age);
-        System.out.println(age);
-       model.addAttribute("height", height);
-        System.out.println(height);
-       model.addAttribute("weight", weight);
-       model.addAttribute("gender", gender);
-        System.out.println(gender);
+    public String macroCounter(Model model, @RequestParam(name = "activity-lv") String activity, @RequestParam(name = "goal") String goal, @RequestParam(name = "age") String age, @RequestParam(name = "gender") String gender, @RequestParam(name = "height") String height, @RequestParam(name = "weight") String weight) {
+        model.addAttribute("age", age);
+        model.addAttribute("height", height);
+        model.addAttribute("weight", weight);
+        model.addAttribute("gender", gender);
         model.addAttribute("activityLv", activity);
-        System.out.println(activity);
         model.addAttribute("goal", goal);
-        System.out.println(goal);
-        model.addAttribute("KEY",KEY);
+        model.addAttribute("KEY", KEY);
         model.addAttribute("MACRO_HOST", MACRO_HOST);
-        return "calculator";
+        return "calculator/calculator";
     }
 }
