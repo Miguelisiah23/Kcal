11175053a7ceb36df2f452cb6cce375b7616599f
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-10-09 12:02:27 -0500

    crunching numbers! success

--- src/main/java/com/romero/kcal/controllers/CalculatorController.java
+++ src/main/java/com/romero/kcal/controllers/CalculatorController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CalculatorController {
@@ -35,22 +32,28 @@ public class CalculatorController {
     }
 
     @GetMapping("/macros")
-    public String macros(Model model){
+    public String macros(){
 
         return "calculator/macros";
     }
 
     @PostMapping("/macros")
-    public String macros(@ModelAttribute MacroNutrients macros) {
+    public String macros(String protein, String carbs, String fat, String diet, String goal, int calories) {
         UserWithRole loggedin = (UserWithRole) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         AppUser user = usersDao.findByUsername(loggedin.getUsername());
-        System.out.println(macros.getCalories());
-        System.out.println(macros.getCarbs());
-        System.out.println(macros.getDiet());
-        System.out.println(macros.getFat());
+        MacroNutrients macros = new MacroNutrients();
+        macros.setProtein(Integer.parseInt(protein));
+        macros.setCarbs(Integer.parseInt(carbs));
+        macros.setFat(Integer.parseInt(fat));
+        macros.setDiet(diet);
+        macros.setCalories(calories);
+        macros.setGoal(goal);
         System.out.println(macros.getProtein());
-        System.out.println(macros.getGoal()
-        );
+        System.out.println(macros.getCarbs());
+        System.out.println(macros.getFat());
+        System.out.println(macros.getCalories());
+        System.out.println(macros.getDiet());
+        System.out.println(macros.getGoal());
 
 
         return "redirect:/macros";
--- src/main/resources/static/js/macro-api.js
+++ src/main/resources/static/js/macro-api.js
@@ -39,7 +39,7 @@ function getMacros() {
                 let lfCarbs = Math.floor(response.data.lowfat.carbs);
                 let lfFat = Math.floor(response.data.lowfat.fat);
 
-                document.querySelector("#results").innerHTML =
+                document.querySelector("#results").innerHTML +=
                     `<div class="text-center">
                     <h1>Results</h1>
                     <h5>Daily Caloric intake: ${kcal}kcal</h5>
@@ -52,7 +52,7 @@ function getMacros() {
                      <h6>*Daily intake</h6>
                      <p>Protein: <span class="protein">${bProtein}</span>g</p> 
                      <p>Carbs: <span class="carbs">${bCarbs}</span>g</p>      
-                     <p>Fat: <span class="fat"> ${bFat}</span>g</p>  
+                     <p>Fat: <span class="fat">${bFat}</span>g</p>  
                      <button class="btn btn-dark save" type="button">Save</button>
                    </div>
                    
@@ -61,16 +61,16 @@ function getMacros() {
                     <h6>*Daily intake</h6>
                     <p>Protein: <span class="protein">${hProtein}</span>g</p>      
                     <p>Carbs: <span class="carbs">${hCarbs}</span>g</p>      
-                    <p>Fat: <span class="fat"> ${hFat}</span>g</p>      
+                    <p>Fat: <span class="fat">${hFat}</span>g</p>      
                     <button class="btn btn-dark save" type="button">Save</button>
                    </div>
                    
                    <div class="diet">
                      <h4 class="diet-name">Low Carb</h4>
                      <h6>*Daily intake</h6>
-                     <p>Protein: <span class="protein">${lcProtein}g</span</p>      
+                     <p>Protein: <span class="protein">${lcProtein}</span>g</p>      
                      <p>Carbs: <span class="carbs">${lcCarbs}</span>g</p>      
-                     <p>Fat: <span class="fat"> ${lcFat}</span>g</p>   
+                     <p>Fat: <span class="fat">${lcFat}</span>g</p>   
                      <button class="btn btn-dark save" type="button">Save</button>   
                    </div>
                    <div class="diet">
@@ -78,28 +78,39 @@ function getMacros() {
                      <h6>*Daily intake</h6>
                      <p>Protein: <span class="protein">${lfProtein}</span>g</p>      
                      <p>Carbs: <span class="carbs">${lfCarbs}</span>g</p>      
-                     <p>Fat: <span class="fat"> ${lfFat}</span>g</p>  
+                     <p>Fat: <span class="fat">${lfFat}</span>g</p>  
                      <button class="btn btn-dark save" type="button">Save</button>    
                    </div>
                    </div>`
 
             let saveButtons = document.querySelectorAll(".save");
-            let proteins = document.querySelectorAll(".protein");
-            let carbs = document.querySelectorAll(".carbs");
-            let fats = document.querySelectorAll(".fat");
-            let diets = document.querySelectorAll(".diet-name");
+
 
             let i = 0;
             for(let save of saveButtons){
                 console.log(i);
+                let proteins = document.querySelectorAll(".protein").item(i);
+                let carbs = document.querySelectorAll(".carbs").item(i);
+                let fats = document.querySelectorAll(".fat").item(i);
+                let diets = document.querySelectorAll(".diet-name").item(i);
                 save.addEventListener("click",()=>{
 
-                    console.log(proteins[0].innerHTML);
-                    console.log(carbs[i].innerHTML);
-                    console.log(fats[i].innerHTML);
-                    console.log(diets[i].innerHTML);
+                    console.log(proteins.innerHTML);
+                    console.log(carbs.innerHTML);
+                    console.log(fats.innerHTML);
+                    console.log(diets.innerHTML);
                     console.log(kcal);
                     console.log(goal);
+
+                    document.querySelector("#save-form").innerHTML +=
+                        `<input type="hidden" name="diet" value="${diets.innerHTML}">
+                        <input type="hidden"  name="goal" value="${goal}">
+                        <input type="hidden"  name="calories" value="${kcal}">
+                        <input type="hidden"  name="protein" value="${proteins.innerHTML}">
+                        <input type="hidden"  name="carbs" value="${carbs.innerHTML}">
+                        <input type="hidden"  name="fat" value="${fats.innerHTML}">`;
+
+                    document.querySelector("#save-form").submit();
 
 
 
